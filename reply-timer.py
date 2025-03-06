5d5769f815231a3bf22de4e1a1e0423bc8b7d98d
Wenshan Xiong <wenshan.xiong@reddit.com>
2025-03-06 11:55:30 -0600

    refine scan logic

--- reply-timer.py
+++ reply-timer.py
@@ -4,11 +4,11 @@ import cred
 from datetime import datetime, timedelta, UTC
 
 # Subreddit to monitor
-SUBREDDIT_NAME = "reply_timer_test"
-# SUBREDDIT_NAME = "whereintheworld"
+# SUBREDDIT_NAME = "reply_timer_test"
+SUBREDDIT_NAME = "whereintheworld"
 
 # Time threshold
-CHECK_MINS = 1
+CHECK_MINS = 60 * 10
 
 def authenticate():
     """Authenticate with Reddit API."""
@@ -26,12 +26,18 @@ def check_posts_for_op_reply(reddit):
     current_time = datetime.now(UTC)
 
     for post in subreddit.new(limit=50):  # Adjust limit as needed
-        post_created_time = datetime.fromtimestamp(post.created_utc, UTC)
-        if (current_time - post_created_time) > timedelta(minutes=CHECK_MINS):
-            op_replied = any(comment.author == post.author for comment in post.comments.list())
-            if post.comments.list() and not op_replied:
-                print(f"OP did not reply in time: {post.title}. url: {post.url}")
-                post.report(reason="")
+        has_comment = False
+        has_op_reply = False
+        for comment in post.comments:
+            comment_created_time = datetime.fromtimestamp(comment.created_utc, UTC)
+            if comment.author != post.author:
+                if (current_time - comment_created_time) > timedelta(minutes=CHECK_MINS):
+                    has_comment = True
+                has_op_reply = has_op_reply or any(reply.author == post.author for reply in comment.replies.list())
+
+        if has_comment and not has_op_reply:
+            print(f"OP did not reply in time: {post.title}. url: {post.url}")
+            post.report(reason="")
 
 def main():
     reddit = authenticate()
